2fd3abefd33c2db19a02579a6ad45ed0ff17bb34
Michael Allen <mikevocalz@aol.com>
2021-10-11 23:18:34 -0400

    add files

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NativeBaseProvider, Text, Box } from 'native-base';
+
+export default function App() {
+  // 2. Use at the root of your app
+  return (
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
