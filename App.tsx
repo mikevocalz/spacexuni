5fa816c03f85895061f6c22074ed3a8f6d405292
Michael Allen <mikevocalz@aol.com>
2021-10-12 00:25:40 -0400

    add macos

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Platform } from 'react-native'
 import { NativeBaseProvider, Text, Box } from 'native-base';
+import { height } from 'styled-system';
 
 export default function App() {
   // 2. Use at the root of your app
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
+      <Box flex={1} bg="#fff"
+        alignItems="center"
+        justifyContent="center" >
+        <Text >Open up App.js to start working on your app!</Text>
       </Box>
     </NativeBaseProvider>
   );
